c8ffe15aa4576eec26f8f0000de87ad31f4ac7f4
epstlight <heewkk@gmail.com>
2020-02-27 16:00:10 +0900

    test3

--- back/Alcohol/src/main/java/com/rest/config/emailAuth/EmailConfig.java
+++ back/Alcohol/src/main/java/com/rest/config/emailAuth/EmailConfig.java
@@ -14,18 +14,18 @@ import org.springframework.stereotype.Component;
 public class EmailConfig {
     Properties properties = new Properties();
 
-    @Value("${mail.smtp.port}")
-    private int port;
-    @Value("${mail.smtp.socketFactory.port}")
-    private int socketPort;
-    @Value("${mail.smtp.auth}")
-    private boolean auth;
-    @Value("${mail.smtp.starttls.enable}")
-    private boolean starttls;
-    @Value("${mail.smtp.starttls.required}")
-    private boolean startlls_required;
-    @Value("${mail.smtp.socketFactory.fallback}")
-    private boolean fallback;
+//    @Value("mail.smtp.port")
+//    private String port;
+//    @Value("mail.smtp.socketFactory.port")
+//    private String socketPort;
+//    @Value("mail.smtp.auth")
+//    private boolean auth;
+//    @Value("mail.smtp.starttls.enable")
+//    private boolean starttls;
+//    @Value("mail.smtp.starttls.required")
+//    private boolean startlls_required;
+//    @Value("mail.smtp.socketFactory.fallback")
+//    private boolean fallback;
 //    @Value("${mail.smtp.username}")
 //    private String username;
 //    @Value("${mail.smtp.password}")
@@ -38,12 +38,12 @@ public class EmailConfig {
         javaMailSender.setHost("smtp.gmail.com");
         javaMailSender.setUsername("heewkk@gmail.com");
         javaMailSender.setPassword("hoxwbxvcjtqpoodm");
-        javaMailSender.setPort(port);
-        properties.put("mail.smtp.socketFactory.port", socketPort);
-        properties.put("mail.smtp.auth", auth);
-        properties.put("mail.smtp.starttls.enable", starttls);
-        properties.put("mail.smtp.starttls.required", startlls_required);
-        properties.put("mail.smtp.socketFactory.fallback", fallback);
+        javaMailSender.setPort(465);
+        properties.put("mail.smtp.socketFactory.port", 465);
+        properties.put("mail.smtp.auth", true);
+        properties.put("mail.smtp.starttls.enable", true);
+        properties.put("mail.smtp.starttls.required", true);
+        properties.put("mail.smtp.socketFactory.fallback", false);
         properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         javaMailSender.setJavaMailProperties(properties);
         javaMailSender.setDefaultEncoding("UTF-8");
--- back/Alcohol/src/main/java/com/rest/config/jwtAuth/JwtTokenProvider.java
+++ back/Alcohol/src/main/java/com/rest/config/jwtAuth/JwtTokenProvider.java
@@ -28,7 +28,7 @@ import lombok.RequiredArgsConstructor;
 @PropertySource("classpath:application.yml")
 public class JwtTokenProvider {
 
-	@Value("${spring.jwt.secret}")
+	@Value("spring.jwt.secret")
 	private String secretKey;
 
 	private long tokenValidMilisecond = 1000L * 60 * 60;
